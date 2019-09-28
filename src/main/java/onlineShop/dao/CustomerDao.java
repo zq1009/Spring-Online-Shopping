eadf73e2c4daf9c224d21db8572c0236f2e56e56
Qin Zeng <qinzeng@usc.edu>
2019-09-28 16:18:28 -0700

    upload

---
+++ src/main/java/onlineShop/dao/CustomerDao.java
@@ -0,0 +1,11 @@
+package onlineShop.dao;
+
+import onlineShop.model.Customer;
+
+//data access object
+public interface CustomerDao {
+    
+    void addCustomer(Customer customer);
+
+    Customer getCustomerByUserName(String userName);
+}
